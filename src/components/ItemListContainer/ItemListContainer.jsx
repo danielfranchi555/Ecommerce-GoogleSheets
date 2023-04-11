a52eafb12a60e3626c1cf0f42e79bc9a349cf0a3
danielfranchi <danielfranchi3@gmail.com>
2023-04-11 16:24:28 -0300

    commit

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -96,7 +96,7 @@ export const ItemListContainer = () => {
              </Stack>
              )):
            info.map((item)=>(
-        <Stack  boxShadow='xl' p='0' rounded='md'   w='180px'   h='auto' >
+        <Stack  boxShadow='xl' p='0' rounded='md'   w='auto'   h='auto' >
         <Stack  p={2} w={{base:'180px',md:'auto'}}  border='solid 1px white' borderRadius='10px'  flex={1}  justifyContent='center' align='center'   alignItems='center' >
           <Image
             src={item.image}
